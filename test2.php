034f13d1887c088e6f1f8fbcc8ae137a641bf27a
Juan Daniel <juankycabj05@gmail.com>
2026-03-26 11:46:46 -0300

    test 2 con muestra de API funcionando

--- test2.php
+++ test2.php
@@ -1,17 +1,12 @@
 <?php
-$autoload = __DIR__ . '/vendor/autoload.php';
-echo "existe autoload: " . (file_exists($autoload) ? 'SI' : 'NO') . "<br>";
 
-$classLoader = __DIR__ . '/vendor/composer/ClassLoader.php';
-echo "existe ClassLoader: " . (file_exists($classLoader) ? 'SI' : 'NO') . "<br>";
+require __DIR__ . '/vendor/autoload.php';
 
-$response = __DIR__ . '/app/core/Response.php';
-echo "existe Response.php: " . (file_exists($response) ? 'SI' : 'NO') . "<br>";
+use App\Core\Response;
 
-// Intentar cargar solo el autoload
-try {
-    require $autoload;
-    echo "autoload cargado: OK<br>";
-} catch (Throwable $e) {
-    echo "error: " . $e->getMessage() . "<br>";
-}
+$response = new Response([
+    "status" => "ok",
+    "mensaje" => "API funcionando"
+]);
+
+$response->send();
