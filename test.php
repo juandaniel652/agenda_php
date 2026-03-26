e0b6708374ba54b7a130d31d9fc7a78d4415caf6
Juan Daniel <juankycabj05@gmail.com>
2026-03-26 14:53:12 -0300

    Verifiar tes2

--- test.php
+++ test.php
@@ -1,9 +1,18 @@
 <?php
-require __DIR__ . '/vendor/autoload.php';
+require_once '/home2/androsnet/public_html/api/vendor/autoload.php';
 
+// Ver qué paths tiene registrado el autoloader para App\
+$loader = require '/home2/androsnet/public_html/api/vendor/autoload.php';
+$prefixes = $loader->getPrefixesPsr4();
+echo '<pre>';
+print_r($prefixes['App\\'] ?? 'NO ENCONTRADO');
+echo '</pre>';
+
+// Verificar si el archivo existe físicamente
+$file = '/home2/androsnet/public_html/api/app/core/Response.php';
+echo "Response.php existe: " . (file_exists($file) ? 'SI' : 'NO') . "<br>";
+
+// Intentar cargarlo directamente
+require_once $file;
 use App\Core\Response;
-
-echo json_encode(['test' => 'autoload ok']);
-
-$r = new Response();
-echo json_encode(['class' => 'Response ok']);
+echo "Response cargado: OK<br>";
